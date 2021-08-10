0e66ef2149cc2087e8381a01049a9af0a9059485
Fellippe Roberto Bione <65615589+frbione@users.noreply.github.com>
2021-08-10 11:02:44 -0300

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -7,7 +7,7 @@ $subject = $_POST['subject'];
 $mailFrom = $_POST['email'];
 $message = $_POST['message'];
 
-$mailTo = "fbione@id.uff.br";
+$mailTo = "frbione@live.it";
 $headers = "From: My Portfolio Website ".$mailFrom;
 $txt = "You have received an Email from ".$name.".\n\n".$message;
 
